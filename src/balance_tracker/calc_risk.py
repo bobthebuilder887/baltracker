a282e14be242efe219fb3f4760994301f7602266
e <e@es-MacBook-Pro.home>
2024-11-27 15:40:49 +0100

    added function with common betting scenarios

--- src/balance_tracker/calc_risk.py
+++ src/balance_tracker/calc_risk.py
@@ -11,3 +11,28 @@ def kelly_criterion_bin(pr_win, frac_loss, frac_gain):
     fraction = pr_win / frac_loss - pr_loss / frac_gain
 
     return fraction
+
+
+def optimal_bets() -> None:
+    kelly_frac = 0.3
+
+    # Random Pump fun
+    frac_loss = 0.7
+    pr_win = 0.1
+    frac_gain = 5
+    print("Random coin on Pump.Fun or similar")
+    print(risk_estimate(pr_win, frac_loss, frac_gain, kelly_frac))
+
+    # High conviction, highly liquid bets with more limited upside
+    frac_loss = 0.4
+    pr_win = 0.7
+    frac_gain = 2
+    print("High conviction, high cap")
+    print(risk_estimate(pr_win, frac_loss, frac_gain, kelly_frac))
+
+    # High conviction, highly illiquid bets with unlimited upside but lower pr
+    frac_loss = 0.7
+    pr_win = 0.3
+    frac_gain = 5
+    print("High conviction, low cap")
+    print(risk_estimate(pr_win, frac_loss, frac_gain, kelly_frac))
